ad43b81bd9fc75c0b0a893caa60f2a371a6f1074
Erika-eni <erikaafedrigo@gmail.com>
2025-11-11 08:09:29 -0300

    feat:adicionando-meta-model-app-endereco

--- sistemaVacinacaoapp/apps/enderecos/models.py
+++ sistemaVacinacaoapp/apps/enderecos/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
-# Create your models here.
-
 class Endereco(models.Model):
     rua = models.CharField('Rua', max_length=200)
     cidade = models.CharField('Cidade', max_length=100)
     estado = models.CharField('Estado',max_length=2)
     bairro = models.CharField('Bairro', max_length=100)
 
+    class Meta:
+        verbose_name = 'Enderecos'
+        verbose_name_plural = 'Enderecos'
+        ordering =['id']
+
     def __str__(self):
-        return f"{self.rua}, {self.bairro}, {self.cidade}-{self.estado}"
+        return f"{self.rua}, {self.bairro}. {self.cidade}-{self.estado}"
