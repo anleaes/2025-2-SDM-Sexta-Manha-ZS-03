d8ae19dc93af90f6383e740930580a78a5145a18
Erika-eni <erikaafedrigo@gmail.com>
2025-11-11 09:12:23 -0300

    feat:serializer-app-cartao

--- sistemaVacinacaoapp/apps/cartoes/serializer.py
+++ sistemaVacinacaoapp/apps/cartoes/serializer.py
@@ -1,2 +1,7 @@
 from .models import Cartao
 from rest_framework import serializers
+
+class CartaoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cartao
+        fields = '__all__'
