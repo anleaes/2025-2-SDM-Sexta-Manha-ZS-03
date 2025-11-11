212dce7cf9eccbd16dc1ea015106b817d581517b
Erika-eni <erikaafedrigo@gmail.com>
2025-11-11 09:56:37 -0300

    feat:definindo-model-app-cidadaos

--- sistemaVacinacaoapp/apps/cidadaos/models.py
+++ sistemaVacinacaoapp/apps/cidadaos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Cidadao(models.Model):
+    endereco = models.ForeignKey('enderecos.Enderecos', on_delete=models.CASCADE)
+    carteirinha = models.OneToOneField('cartao.Cartao', on_delete=models.CASCADE)
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=15)
+    nascimento = models.DateField()
+    idade = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
