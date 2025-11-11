50939c4e7e8f63f3e7db4c383f19f28b99ebe5d7
Erika-eni <erikaafedrigo@gmail.com>
2025-11-11 09:16:31 -0300

    feat:viewSet-app-cartao

--- sistemaVacinacaoapp/apps/cartoes/views.py
+++ sistemaVacinacaoapp/apps/cartoes/views.py
@@ -3,4 +3,6 @@ from rest_framework import viewsets
 from .models import Cartao
 from .serializer import CartaoSerializer
 
-
+class CartaoViewSet(viewsets.ModelViewSet):
+    queryset = Cartao.objects.all()
+    serializer_class = CartaoSerializer
