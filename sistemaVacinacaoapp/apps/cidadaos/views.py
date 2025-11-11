2549c0938ed3a71461d35a38f689e27a02defd7e
Erika-eni <erikaafedrigo@gmail.com>
2025-11-11 20:51:51 -0300

    feat:viewSet-app-cidadaos

--- sistemaVacinacaoapp/apps/cidadaos/views.py
+++ sistemaVacinacaoapp/apps/cidadaos/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Cidadao
 from .serializer import CidadaoSerializer
+
+class CidadaoViewSet(viewsets.ModelViewSet):
+    queryset = Cidadao.objects.all()
+    serializer_class = CidadaoSerializer
