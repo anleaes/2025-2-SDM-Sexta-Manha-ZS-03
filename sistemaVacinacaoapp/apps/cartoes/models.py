b82638fa2618eb14169ca8ae3da94f94cf0216fc
Erika-eni <erikaafedrigo@gmail.com>
2025-11-11 09:07:05 -0300

    feat:definindo-model-app-cartao

--- sistemaVacinacaoapp/apps/cartoes/models.py
+++ sistemaVacinacaoapp/apps/cartoes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Cartao(models.Model):
+    numero = models.CharField('Numero', max_length=50, min_length=10, unique=True)
+    criacao = models.DateField()
+    zona = models.CharField('Zona', max_length=30, choices=[
+        ('SUL', 'Sul'),
+        ('NORTE', 'Norte'),
+        ('LESTE', 'Leste'),
+        ('OESTE', 'Oeste'),
+        ])
+    status = models.CharField('Status', max_length=30, choices=[
+        ('ATIVO', 'Ativo'),
+        ('BLOQUEADO', 'Bloqueado'),
+        ])
+
+    def __str__(self):
+        return self.numero
