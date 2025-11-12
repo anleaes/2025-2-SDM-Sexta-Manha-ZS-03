5c191e04e1145d1abbbb3bc08a3cf0827cd9e02c
Erika-eni <erikaafedrigo@gmail.com>
2025-11-12 07:37:12 -0300

    feat:serializer-app-fabricantes

--- sistemaVacinacaoapp/apps/fabricantes/serializer.py
+++ sistemaVacinacaoapp/apps/fabricantes/serializer.py
@@ -1,2 +1,7 @@
 from .models import Fabricante
 from rest_framework import serializers
+
+class FabricanteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Fabricante
+        fields = '__all__'
