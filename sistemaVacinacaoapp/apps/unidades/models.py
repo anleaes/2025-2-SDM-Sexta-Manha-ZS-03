56e84c94bcc487947cd6b6cca510f720f6ceb469
Erika-eni <erikaafedrigo@gmail.com>
2025-11-11 21:29:19 -0300

    feat:definindo-model-app-unidades

--- sistemaVacinacaoapp/apps/unidades/models.py
+++ sistemaVacinacaoapp/apps/unidades/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from enderecos.models import Enderecos
 
-# Create your models here.
+class Unidade(models.Model):
+    endereco = models.OneToOneField(Enderecos, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=100)
+    zona = models.CharField('Status', max_length=30, choices=[
+        ('URBANA', 'Urbana'),
+        ('RURAL', 'Rural'),
+        ])
+    telefone = models.CharField(max_length=20)
+    tipo = models.CharField('Status', max_length=30, choices=[
+        ('POSTO', 'Posto de Saúde'),
+        ('HOSPITAL', 'Hospital'),
+        ('CLINICA', 'Clínica'),
+        ])
+    
+    def __str__(self):
+        return self.nome
