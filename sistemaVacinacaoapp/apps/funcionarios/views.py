adc58d377818d90387509ab04dcdabd6734b7da0
Erika-eni <erikaafedrigo@gmail.com>
2025-11-12 08:36:16 -0300

    feat:viewSet-app-funcionarios

--- sistemaVacinacaoapp/apps/funcionarios/views.py
+++ sistemaVacinacaoapp/apps/funcionarios/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Funcionario
 from .serializer import FuncionarioSerializer
+
+class FuncionarioViewSet(viewsets.ModelViewSet):
+    queryset = Funcionario.objects.all()
+    serializer_class = FuncionarioSerializer
