78e3fcc2bcbe58cce1a3c2c20ef821698d73fe23
Erika-eni <erikaafedrigo@gmail.com>
2025-11-12 08:01:03 -0300

    feat:defininfo-model-app-vacinas

--- sistemaVacinacaoapp/apps/vacinas/models.py
+++ sistemaVacinacaoapp/apps/vacinas/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from fabricantes.models import Fabricante
 
-# Create your models here.
+class Vacina(models.Model):
+    fabricante = models.ForeignKey(Fabricante, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=100)
+    doses = models.IntegerField()
+    intervaloDias = models.IntegerField()
+    doenca = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
