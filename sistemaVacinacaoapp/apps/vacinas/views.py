d3877b53fd66218933a57a3ec55815992df118b2
Erika-eni <erikaafedrigo@gmail.com>
2025-11-12 08:08:31 -0300

    feat:viewSet-app-vacinas

--- sistemaVacinacaoapp/apps/vacinas/views.py
+++ sistemaVacinacaoapp/apps/vacinas/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Vacina
 from .serializer import VacinaSerializer
+
+class VacinaViewSet(viewsets.ModelViewSet):
+    queryset = Vacina.objects.all()
+    serializer_class = VacinaSerializer
