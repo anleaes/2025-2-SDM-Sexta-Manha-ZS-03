e640f2ea38b06565d03b62047c0e9ddbcd92022b
Erika-eni <erikaafedrigo@gmail.com>
2025-11-12 08:31:40 -0300

    feat:definindo-model-app-funcionarios

--- sistemaVacinacaoapp/apps/funcionarios/models.py
+++ sistemaVacinacaoapp/apps/funcionarios/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from enderecos.models import Endereco
+from unidades.models import Unidade
 
-# Create your models here.
+class Funcionario(models.Model):
+    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
+    unidade = models.ForeignKey(Unidade, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=15)
+    cargo = models.CharField('Cargo', max_length=30, choices=[
+        ('ENFERMEIRO', 'Enfermeiro'),
+        ('MEDICO', 'Médico'),
+        ('ADMINISTRATIVO', 'Administrativo'),
+        ('OUTROS', 'Outros'),
+        ])
+    registro = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.nome}, {self.cargo}."
