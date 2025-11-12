8d2a81ee745bb1a9bcf4de14d0d48f91f6f51281
Erika-eni <erikaafedrigo@gmail.com>
2025-11-12 08:03:59 -0300

    feat:serializer-app-vacina

--- sistemaVacinacaoapp/apps/vacinas/serializer.py
+++ sistemaVacinacaoapp/apps/vacinas/serializer.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 from .models import Vacina
+
+class VacinaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vacina
+        fields = '__all__'
