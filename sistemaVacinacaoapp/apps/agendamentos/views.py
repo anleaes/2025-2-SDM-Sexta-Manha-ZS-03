89ca25c2de283329cb2d3e96d0e3deb534b8f4df
Erika-eni <erikaafedrigo@gmail.com>
2025-11-12 22:03:38 -0300

    feat:viewSet-app-serializer

--- sistemaVacinacaoapp/apps/agendamentos/views.py
+++ sistemaVacinacaoapp/apps/agendamentos/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Agendamento
 from .serializer import AgendamentoSerializer
+
+class AgendamentoViewSet(viewsets.ModelViewSet):
+    queryset = Agendamento.objects.all()
+    serializer_class = AgendamentoSerializer
