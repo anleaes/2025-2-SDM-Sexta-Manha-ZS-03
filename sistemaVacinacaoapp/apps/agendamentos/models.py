0e15ed2be8dc04963c244fd5cbdb4e382914bc51
Erika-eni <erikaafedrigo@gmail.com>
2025-11-12 21:56:45 -0300

    feat:definindo-model-app-agendamentos

--- sistemaVacinacaoapp/apps/agendamentos/models.py
+++ sistemaVacinacaoapp/apps/agendamentos/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Agendamento(models.Model):
+    unidade = models.ForeignKey('unidade.Unidade', on_delete=models.CASCADE)
+    vacina = models.ForeignKey('vacina.Vacina', on_delete=models.CASCADE)
+    cidadao = models.ForeignKey('cidadao.Cidadao', on_delete=models.CASCADE)
+    funcionario = models.ForeignKey('funcionario.Funcionario', on_delete=models.CASCADE)
+
+    dataMarcada = models.DateField()
+    status = models.CharField('Status', max_length=30, choices=[
+        ('AGENDADO', 'Agendado'),
+        ('CONCLUIDO', 'Concluído'),
+        ('CANCELADO', 'Cancelado'),
+        ])
+    observacoes = models.TextField()
+    prioridade = models.CharField('Status', max_length=30, choices=[
+        ('BAIXA', 'Baixa'),
+        ('MEDIA', 'Média'),
+        ('ALTA', 'Alta'),
+        ])
+
+    def __str__(self):
+        return f"{self.cidadao} - {self.dataMarcada} - {self.status}. Vacina: {self.vacina}"
